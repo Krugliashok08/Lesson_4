0298ca6323d03b1f2fba668d1d8742a4736cc98e
krugliashok08 <annakm803@gmail.com>
2024-02-22 20:10:16 +0300

    Lesson_4

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+Employee employee = new Employee ("Ivanov Ivan","Engineer", "Ivan@mail.ru", "4124555",50000,30);
+
+    System.out.println(employee);
+
+    Employee[] emArray = new Employee[5];
+        emArray[0] = new Employee("Ivanov Ivan", "Engineer", "Ivan@mail.ru", "4124555", 50000, 30);
+        emArray[1] = new Employee("Petrov Peter", "Director", "Peter@mail.ru", "4124550", 55000, 35);
+        emArray[2] = new Employee("Sidorov Slava", "Manager", "Slava@mail.ru", "4124551", 51000, 29);
+        emArray[3] = new Employee("Putin Vladimir", "Controller", "Vladimir@mail.ru", "4124552", 70000, 44);
+        emArray[4] = new Employee("Medvedev Dmitrii", "QA Leader", "Dmitrii@mail.ru", "4124554", 65000, 38);
+        for (int i = 0; i < emArray.length; i++) {
+            System.out.println("Name: " + emArray[i].getName());
+            System.out.println("Position: " + emArray[i].getPosition());
+            System.out.println("Email: " + emArray[i].getEmail());
+            System.out.println("Phone: " + emArray[i].getPhone());
+            System.out.println("Salary: " + emArray[i].getSalary());
+            System.out.println("Age: " + emArray[i].getAge());
+
+
+        }
+
+
+    }
+
+}
